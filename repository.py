b92c43e054b760c893b4fe166523f225df7ba276
Josh <joshua@durhamgardens.co.uk>
2025-02-04 08:30:33 +0000

    add insert and lookup to repository

--- repository.py
+++ repository.py
@@ -10,7 +10,33 @@ class Repository:
         with sqlite3.connect(self.database) as connection:
             cursor = connection.cursor()
             cursor.execute(
-                f"CREATE TABLE IF NOT EXISTS {self.table} " +
-                "(name TEXT PRIMARY KEY, artist TEXT PRIMARY KEY, file TEXT)"
+                f"""
+                CREATE TABLE IF NOT EXISTS {self.table} (
+                    name TEXT,
+                    artist TEXT,
+                    file TEXT,
+                    PRIMARY KEY (name, artist)
+                )
+                """
             )
             connection.commit()
+
+    def insert(self, js):
+        with sqlite3.connect(self.database) as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                f"INSERT INTO {self.table} (name, artist, file) VALUES (?, ?, ?) ",
+                (js["name"], js["artist"], js["file"])
+            )
+            connection.commit()
+    
+    def lookup(self, name, artist):
+        with sqlite3.connect(self.database) as connection:
+            cursor = connection.cursor()
+            cursor.execute(
+                f"SELECT file FROM {self.table} WHERE (name, artist) = (?, ?)",
+                (name, artist), 
+            )
+            row = cursor.fetchone()
+            if row:
+                return { "name":row[0], "artist":row[1], "file":row[2] }
